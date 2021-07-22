267c086d9866b2a9959c3103a58e65f5ac3c0bc4
dynamonda <v7gj9kk@gmail.com>
2021-07-22 12:46:20 +0900

    マイグレーションによる変更

---
+++ database/migrations/2021_07_22_033926_update_hello_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateHelloTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hello', function (Blueprint $table) {
+            // カラム名変更
+            $table->renameColumn('str_2', 'str_opt');
+            
+            // 新しくカラム追加(null許容)
+            $table->integer('count')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hello', function (Blueprint $table) {
+            //
+        });
+    }
+}
