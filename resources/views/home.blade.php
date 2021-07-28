e6e50c79ebd24f5f3c75f529f04c329bbe103fbd
dynamonda <v7gj9kk@gmail.com>
2021-07-29 08:41:36 +0900

    UserのIDや名前、コメントを表示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,21 @@
     @section〜@endsection で自分固有のパーツをつける   
 --}}
 
+{{-- Bootstrapのグリッドシステム
+・画面の横を12分割していくつ領域を使うかを指定する
+・sm, md, lg, xlひとまずmd(PC用)を使っておけばベター
+
+--}}
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <nav>
+            <ul class="nav flex-column">
+                <li class="nav-item">item1</li>
+                <li class="nav-item">item2</li>
+            </ul>
+        </nav>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -22,6 +34,25 @@
 
                     {{--　__(hogehoge)はヘルパ関数。中身を展開する --}}
                     {{ __('You are logged in!') }}
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">ログイン情報</div>
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Key</th>
+                                <th scope="col">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td>ID</td><td>{{ __(Auth::id()) }}</td></tr>
+                            <tr><td>名前</td><td>{{ __(Auth::user()->name) }}</td></tr>
+                            <tr><td>メールアドレス</td><td>{{ __(Auth::user()->email) }}</td></tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
