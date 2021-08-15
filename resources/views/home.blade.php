3966cde19271d4e4001060bcf53cdb6de7a9400d
dynamonda <v7gj9kk@gmail.com>
2021-08-16 05:52:43 +0900

    Vueに変数を渡す

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,76 +13,79 @@
 --}}
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <nav>
-            <ul class="nav flex-column">
-                <li class="nav-item">item1</li>
-                <li class="nav-item">item2</li>
-            </ul>
-        </nav>
-        <div class="col-md-8">
-            {{--
-                cardはどこから来た？
+<div id="app">
+    <div class="container">
+        <div class="row justify-content-center">
+            <nav>
+                <ul class="nav flex-column">
+                    <li class="nav-item">item1</li>
+                    <li class="nav-item">item2</li>
+                </ul>
+            </nav>
+            <div class="col-md-8">
+                {{--
+                    cardはどこから来た？
 
-                extends('layouts.app') で views/layouts/app.blade.php を読み込む
-                    -> asset('js/app.js') で js/app.js を読み込む
-                    -> require('./bootstrap'); で Bootstrap を読み込む
-                
-                    Bootstrapで定義（現在は5.0.2がインストールされている）
-            --}}
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                    extends('layouts.app') で views/layouts/app.blade.php を読み込む
+                        -> asset('js/app.js') で js/app.js を読み込む
+                        -> require('./bootstrap'); で Bootstrap を読み込む
+                    
+                        Bootstrapで定義（現在は5.0.2がインストールされている）
+                --}}
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{--　__(hogehoge)はヘルパ関数。中身を展開する --}}
-                    {{ __('You are logged in!') }}
+                        {{--　__(hogehoge)はヘルパ関数。中身を展開する --}}
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card-header">ログイン情報</div>
-                <div class="card-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">Key</th>
-                                <th scope="col">Value</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr><td>ID</td><td>{{$user_id}}</td></tr>
-                            <tr><td>名前</td><td>{{ __($user->name) }}</td></tr>
-                            <tr><td>メールアドレス</td><td>{{ __($user->email) }}</td></tr>
-                        </tbody>
-                    </table>
+                <div class="card">
+                    <div class="card-header">ログイン情報</div>
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Key</th>
+                                    <th scope="col">Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr><td>ID</td><td>{{$user_id}}</td></tr>
+                                <tr><td>名前</td><td>{{ __($user->name) }}</td></tr>
+                                <tr><td>メールアドレス</td><td>{{ __($user->email) }}</td></tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card-header">テキスト入力テスト</div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="testCommentTextarea">コメントテキストエリア</label>
-                            <textarea class="form-control" id="testCommentTextarea" rows="3">{{$profile->comment}}</textarea>
-                            <button type="submit" class="btn btn-primary">保存</button>
-                        </div>
-                    </form>
+                <div class="card">
+                    <div class="card-header">テキスト入力テスト</div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="testCommentTextarea">コメントテキストエリア</label>
+                                <textarea class="form-control" id="testCommentTextarea" rows="3">{{$profile->comment}}</textarea>
+                                <button type="submit" class="btn btn-primary">保存</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+
+                @php
+                    $profilecomm = $profile->comment
+                @endphp
+                <example-component :profile='@json($profile)'></example-component>
+                <card-component></card-component>
             </div>
         </div>
     </div>
-</div>
-
-<div id="app">
-    <example-component></example-component>
-    <card-component></card-component>
 </div>
 @endsection
