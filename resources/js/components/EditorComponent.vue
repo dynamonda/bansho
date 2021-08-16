904f360b920c86238f713dc82cadb09e6619619e
dynamonda <v7gj9kk@gmail.com>
2021-08-16 21:22:25 +0900

    Editorの保存ボタンの位置を変更

--- resources/js/components/EditorComponent.vue
+++ resources/js/components/EditorComponent.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        <div id="editorjs"></div>
+    <div class="card">
+        <div class="card-header">
+            <div class="btn-group float-end">
+                <button class="btn" v-on:click="save">保存</button>
+            </div>
+        </div>
+        <div class="card-body">
+            <div id="editorjs"></div>
+        </div>
     </div>
 </template>
 
 <script>
     import EditorJS from "@editorjs/editorjs";
-    
+
     export default {
         name: "NoteForm",
         data: () => ({
@@ -17,6 +24,14 @@
                 holder: 'editorjs',
             });
         },
-        methods: {}
+        methods: {
+            save(){
+                this.editor.save().then((data) => {
+                    console.log('保存: ' + data);
+                }).catch((err) => {
+                    console.error(err);
+                });
+            }
+        }
     }
 </script>
